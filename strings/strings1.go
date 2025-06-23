8013a61f37f996c6d49b3bd6e68744fe5c1ee78e
Milana <kmilka14@mail.ru>
2025-06-23 14:46:32 +0300

    Final_md

---
+++ strings/strings1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var text string
+	fmt.Println("Введите текст:")
+	fmt.Scanln(&text)
+	var k int
+	for range text {
+		k++
+	}
+	fmt.Println("Количество символов:", k)
+}

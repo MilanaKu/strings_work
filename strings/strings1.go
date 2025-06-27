352bf3412bf2dfb69c84feb6bdb8dc2e2d0c54f4
MilanaKu <kmilka14@mail.ru>
2025-06-28 01:09:05 +0300

    Update strings1.go

--- strings/strings1.go
+++ strings/strings1.go
@@ -1,14 +1,1 @@
-package main
 
-import "fmt"
-
-func main() {
-	var text string
-	fmt.Println("Введите текст:")
-	fmt.Scanln(&text)
-	var k int
-	for range text {
-		k++
-	}
-	fmt.Println("Количество символов:", k)
-}

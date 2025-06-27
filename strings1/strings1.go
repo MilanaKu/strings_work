2fa497fc2aa1c20135b63e817813801654d01054
Milana <kmilka14@mail.ru>
2025-06-28 00:41:02 +0300

    str1_done

---
+++ strings1/strings1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode/utf8"
+)
+
+func main() {
+	fmt.Println("Введите текст:")
+	reader := bufio.NewScanner(os.Stdin)
+	reader.Scan()
+	scanner := reader.Text()
+	fmt.Println(utf8.RuneCountInString(scanner))
+}

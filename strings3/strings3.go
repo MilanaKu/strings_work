ca2621bfd63a5d79d205675d89182c6221e84317
MilanaKu <kmilka14@mail.ru>
2025-06-29 14:20:11 +0300

    Update strings3.go

--- strings3/strings3.go
+++ strings3/strings3.go
@@ -10,17 +10,20 @@ import (
 func capitalizeWords(s string) string {
 	split_s := strings.Split(s, " ")
 	for i, k := range split_s {
-		first_letter := strings.ToUpper(string(k[0]))
-		rest_letters := strings.ToLower(string(k[1:]))
+		runes := []rune(k)
+		first_letter := strings.ToUpper(string(runes[0]))
+		rest_letters := strings.ToLower(string(runes[1:]))
 		split_s[i] = first_letter + rest_letters
 	}
 	return strings.Join(split_s, " ")
 }
-func min() {
+func main() {
 	fmt.Println("Введите текст:")
 	reader := bufio.NewScanner(os.Stdin)
+	reader.Scan()
 	text := reader.Text()
 	changed_text := capitalizeWords(text)
 	fmt.Println(changed_text)
 
 }
+

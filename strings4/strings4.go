f2856df4d2acafa5a01b4d1b29ba9404730c1282
MilanaKu <kmilka14@mail.ru>
2025-06-29 14:56:53 +0300

    Update strings4.go

--- strings4/strings4.go
+++ strings4/strings4.go
@@ -9,16 +9,22 @@ import (
 func check(form string) {
 	var open_bracket int
 	var close_bracket int
+	var correction bool = false
 	for _, k := range form {
 		if k == '(' {
 			open_bracket++
 		}
 		if k == ')' {
 			close_bracket++
+			if open_bracket != close_bracket {
+				correction = true
+			}
 		}
 	}
-	if open_bracket == close_bracket {
+	if open_bracket == close_bracket && correction == false {
 		fmt.Printf("Скобки расставлены верно, %d открывающиеся, %d закрывающиеся\n", open_bracket, close_bracket)
+	} else if open_bracket == close_bracket && correction == true {
+		fmt.Printf("Скобки расставлены в неправильном порядке!")
 	} else {
 		fmt.Printf("Скобки расставлены неправильно, %d открывающиеся, %d закрывающиеся\n", open_bracket, close_bracket)
 	}
